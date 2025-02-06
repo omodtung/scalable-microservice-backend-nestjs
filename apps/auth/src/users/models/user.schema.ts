a8a621d2b266912f204fad28961c94c71a11e19f
TungDT <dothedung131@gmail.com>
2025-02-06 22:37:22 +0700

    user module and small docker file

---
+++ apps/auth/src/users/models/user.schema.ts
@@ -0,0 +1,10 @@
+import { Schema, SchemaFactory, Prop } from '@nestjs/mongoose';
+import { AbstractDocument } from '@app/common';
+@Schema({ versionKey: false })
+export class UserDocument extends AbstractDocument {
+  @Prop()
+  email: string;
+  @Prop()
+  password: string;
+}
+export const UserSchema = SchemaFactory.createForClass(UserDocument);
--- apps/auth/src/users/users.module.ts
+++ apps/auth/src/users/users.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
-
+import { DatabaseModule } from '@app/common';
+import { UserDocument, UserSchema } from './models/user.schema';
+import { LoggerModule } from '@app/common';
+import { UsersRepository } from './users.repository';
 @Module({
+  imports: [
+    DatabaseModule,
+    DatabaseModule.forFeature([
+      { name: UserDocument.name, schema: UserSchema },
+    ]),
+  ],
+
   controllers: [UsersController],
-  providers: [UsersService]
+  providers: [UsersService, UsersRepository],
+  exports: [UsersService],
 })
 export class UsersModule {}
