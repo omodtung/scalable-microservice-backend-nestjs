db8ae6884f859153777e7b71e31708f704e23247
TungDT <dothedung131@gmail.com>
2025-02-12 22:25:39 +0700

    LocalGuard ->validate - > vetify

---
+++ apps/auth/src/strategies/local.strategy.ts
@@ -0,0 +1,17 @@
+import { UsersService } from '../users/users.service';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { Strategy } from 'passport-local';
+@Injectable()
+export class LocalStrategy extends PassportStrategy(Strategy) {
+  constructor(private readonly usersService: UsersService) {
+    super({ usernameField: 'email' });
+  }
+  async validate(email: string, password: string) {
+    try {
+      return await this.usersService.verifyUsers(email, password);
+    } catch (error) {
+      throw new UnauthorizedException('credentials are invalid');
+    }
+  }
+}
